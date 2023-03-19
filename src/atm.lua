0499192950f3c73d86d9b8bfd97d35b49f494fe0
BIueKnight <39872493+BIueKnight@users.noreply.github.com>
2023-03-18 22:12:53 -0500

    ATM client changes: Reorganized code to allow manual UI refresh, cleaned up code and added proper logging
    Changed DisplayMessage function to allow "unskippable" messages that must remain until condition is met.
    Example uses are a "loading" message when connecting to the server (not  implemented) or a "invalid card" message (implemented)
    
    There are points where the program will not continue until response from the server, but UI may still need to be updated, added a manual UpdateUI function to address this.
    
    cleaned up code removing things already declared in apis
    
    code is reorganized to allow for manual ui updates

--- src/atm.lua
+++ src/atm.lua
@@ -1,41 +1,8 @@
 local bank = require("atmBankApi")
-local modem = peripheral.find("modem") or error("No modem attached", 0)
 local monitor = peripheral.find("monitor") or error("No monitor attached", 0)
 local cardDrive = peripheral.wrap("right")
 local interfaceStorage = peripheral.wrap("front")
 local internalStorage = peripheral.wrap("back")
-local opMode = false
-
-local responsePort = 531 + os.getComputerID()
-
-bank.initialize(responsePort)
-
-local coins = {
-    spurs = {
-        nbt = "d3adddbc586c8a708b5e213b206b7687",
-        rate = 1 / 64
-    },
-    bevels = {
-        nbt = "c9f52ce05acf3715bf592eea6edbc450",
-        rate = 1 / 8
-    },
-    sprockets = {
-        nbt = "25a3275f9ecdc11c78648e61e95376b0",
-        rate = 1 / 4
-    },
-    cogs = {
-        nbt = "2442f28a7aec5cf7b09d2c2756caa1a4",
-        rate = 1
-    },
-    crowns = {
-        nbt = "77b458f3adececb55e27a47b4ecb714b",
-        rate = 8
-    },
-    suns = {
-        nbt = "9eabd6c6d7c587c1694fb86d4182cd62",
-        rate = 64
-    }
-}
 
 local interfaceStorageMoney = {
     total = 0,
@@ -47,28 +14,10 @@ local internalStorageMoney = {
 }
 
 local currentUser = nil
-local DisplayedMessage = "Unknown error"
-local screen = "insert" -- insert, info, withdraw, deposit, transfer, balance
-
--- Function to display a message on the monitor
-
-local function displayMessage(message)
-    print("displaying message: " .. message)
-    screen = "info"
-    DisplayedMessage = message or "Unknown error"
-end
-
-local function updateUser()
-    local UUID = bank.getUUID(cardDrive)
-    print(UUID)
-    currentUser = bank.getUser(UUID)
-end
-
-modem.open(responsePort)
 
 local function countCoins(tab, amount)
     if ((tab.total or 0) < amount) then
-        print("Not enough money (" .. tab.total .. " < " .. amount .. ")")
+        if (bank.logging) then print("Not enough money (" .. tab.total .. " < " .. amount .. ")") end
         return 0
     end
     if (tab.total == amount) then
@@ -84,7 +33,7 @@ local function countCoins(tab, amount)
 
     table.sort(tab.detail, function(a, b) return coins[a.coin].rate > coins[b.coin].rate end)
     for _, data in pairs(tab.detail) do
-        local coin = coins[data.coin]
+        local coin = bank.coins[data.coin]
         local count = math.floor((amount - total) / coin.rate)
         if (count > data.count) then
             count = data.count
@@ -103,17 +52,10 @@ local function countCoins(tab, amount)
     return result
 end
 
-local charset = {}
-do -- [0-9a-zA-Z]
-    for c = 48, 57 do table.insert(charset, string.char(c)) end
-    for c = 65, 90 do table.insert(charset, string.char(c)) end
-    for c = 97, 122 do table.insert(charset, string.char(c)) end
-end
-
 local function checkInterfaceStorage()
     interfaceStorageMoney.total = 0
     for slot, item in pairs(interfaceStorage.list()) do
-        for key, coin in pairs(coins) do
+        for key, coin in pairs(bank.coins) do
             if (item.nbt == coin.nbt) then
                 interfaceStorageMoney.detail[slot] = {
                     coin = key,
@@ -128,7 +70,7 @@ end
 local function checkInternalStorage()
     internalStorageMoney.total = 0
     for slot, item in pairs(internalStorage.list()) do
-        for key, coin in pairs(coins) do
+        for key, coin in pairs(bank.coins) do
             if (item.nbt == coin.nbt) then
                 internalStorageMoney.detail[slot] = {
                     coin = key,
@@ -140,31 +82,92 @@ local function checkInternalStorage()
     end
 end
 
-local function deposit(amount)
-    print("Depositing " .. amount .. "C into " .. currentUser.name .. "'s account")
-    checkInterfaceStorage()
-    if interfaceStorageMoney.total < amount then
-        print("Not enough money in interface storage")
-        displayMessage("Not enough money in interface storage")
-        return false
+-- screen variables
+local DisplayedMessage = "Unknown error"
+local screen = "insert" -- insert, info, withdraw, deposit, transfer, balance(main)
+local skipInfo = true -- true if user can continue from current info screen
+monitor.setTextScale(0.5)
+local w, h = monitor.getSize()
+
+-- call to update UI state
+local function updateUI()
+if screen == "info" then
+        if (bank.logging) then print("Info: " .. DisplayedMessage) end
+        monitor.clear()
+        local wrappedErrorMessageLines = require "cc.strings".wrap(DisplayedMessage, w)
+        for i, line in ipairs(wrappedErrorMessageLines) do
+            monitor.setCursorPos(1, i)
+            monitor.write(line)
+        end
+        -- allow going to the main menu only if user can skip this info
+        if skipInfo then
+            monitor.setCursorPos(1, h)
+            monitor.write("Continue")
+        end
+    elseif screen == "main" then
+        checkInterfaceStorage()
+        monitor.clear()
+        monitor.setCursorPos(1, 1)
+        monitor.write(currentUser.name)
+        monitor.setCursorPos(1, 2)
+        monitor.write("Bal: " .. math.floor(currentUser.balance * 100) / 100 .. "C")
+        monitor.setCursorPos(1, 4)
+        monitor.write("deposit " .. math.floor(interfaceStorageMoney.total * 100) / 100 .. "C")
+        monitor.setCursorPos(1, 5)
+        monitor.write("withdraw")
+    elseif screen == "insert" then
+        monitor.clear()
+        monitor.setCursorPos(1, 1)
+        monitor.write("Insert card")
     end
+end
+
+-- Function to display a message on the monitor
+local function displayMessage(message, skip)
+    if (skip == nil) then skip = true end -- skip defaults to true if not passed
+    
+    if (bank.logging) then print("displaying message: " .. message) end
+    screen = "info"
+    skipInfo = skip
+    DisplayedMessage = message or "Unknown error"
+end
+
+local function updateUser()
+    local UUID = bank.getUUID(cardDrive)
+    if (bank.logging) then print(UUID) end
+    displayMessage("Reading Card. Please Wait...", false)
+    updateUI()
+    currentUser = bank.getUser(UUID)
+end
+
+local function deposit(amount)
+    if (bank.logging) then print("Depositing " .. amount .. "C into " .. currentUser.name .. "'s account") end
+    checkInterfaceStorage()
+    -- redundant as countCoins achieves this with extra validation
+    --[[if interfaceStorageMoney.total < amount then
+        if (bank.logging) then print("Not enough money in interface storage")
+        displayMessage("Not enough money in interface storage") end
+        return false
+    end]]--
     local coinSlots = countCoins(interfaceStorageMoney, amount)
     if coinSlots == 0 then
-        print("Not enough coins to make up " .. amount .. "C")
+        if (bank.logging) then print("Not enough coins to make up " .. amount .. "C") end
         displayMessage("Not enough money in interface storage")
         return false
     end
     local response = bank.request("deposit", { name = currentUser.name, amount = amount, cardID = currentUser.cardID })
-    if response.status == "success" then
-        print("Deposited " .. amount .. "C into " .. currentUser.name .. "'s account")
-        displayMessage("Deposited " .. amount .. "C")
+    if (response.status == "success") then
+        -- update user balance locally
+        currentUser.balance = currentUser.balance + amount
         for slot, value in pairs(coinSlots) do
             interfaceStorage.pushItems("back", slot, value.count)
         end
+        if (bank.logging) then print("Deposited " .. amount .. "C into " .. currentUser.name .. "'s account") end
+        displayMessage("Deposited " .. amount .. "C")
         return true
     else
         displayMessage("Failed to deposit " .. amount .. "C")
-        print("Failed to deposit " .. amount .. "C into " .. currentUser.name .. "'s account")
+        if (bank.logging) then print("Failed to deposit " .. amount .. "C into " .. currentUser.name .. "'s account") end
         return false
     end
 end
@@ -173,28 +176,31 @@ local function withdraw(amount)
     checkInternalStorage()
     local coinSlots = countCoins(internalStorageMoney, amount)
     if (coinSlots == 0) then
-        print("Not enough money in internal storage")
+        if (bank.logging) then print("Not enough money in internal storage") end
         bank.alertServer("Not enough money in internal storage")
         displayMessage("Not enough money in internal storage")
         return false
     end
     local response = bank.request("withdraw", { name = currentUser.name, amount = amount, cardID = currentUser.cardID })
-    if response.status == "success" then
+    if (response.status == "success") then
+        -- update user balance locally
+        currentUser.balance = currentUser.balance - amount
         for slot, value in pairs(coinSlots) do
             internalStorage.pushItems("front", slot, value.count)
         end
         displayMessage("Withdrew " .. amount .. "C")
-        print("Withdrew " .. amount .. "C from " .. currentUser.name .. "'s account")
+        if (bank.logging) then print("Withdrew " .. amount .. "C from " .. currentUser.name .. "'s account") end
+        return true
     else
         displayMessage("Failed to withdraw " .. amount .. "C")
-        print("Failed to withdraw " .. amount .. "C from " .. currentUser.name .. "'s account")
+        if (bank.logging) then print("Failed to withdraw " .. amount .. "C from " .. currentUser.name .. "'s account") end
+        return false
     end
 end
 
--- Main loop to listen for incoming requests
+bank.initialize()
 
-monitor.setTextScale(0.5)
-local w, h = monitor.getSize()
+-- Main loop to listen for incoming requests
 local withdrawAmountString = ""
 
 while true do
@@ -203,18 +209,23 @@ while true do
     if e[1] == "modem_message" then
         bank.handleModemRequest(e)
     elseif e[1] == "disk" then
-        screen = "main"
+        updateUser()
+        if not currentUser then
+            displayMessage("Invalid card. Please remove card and insert a valid card.", false)
+        else
+            screen = "main"
+        end
     elseif e[1] == "disk_eject" then
         currentUser = nil
         screen = "insert"
     elseif e[1] == "monitor_touch" then
-        print("Touch: " .. screen)
-        if screen == "info" then
+        if (bank.logging) then print("Touch: " .. screen) end
+        if screen == "info" and skipInfo then
+            -- go back to the main menu only if the user can skip this info
             screen = "main"
-        end
-        if screen == "main" then
+        elseif screen == "main" then
             local x, y = e[3], e[4]
-            print(x, y)
+            if (bank.logging) then print(x, y) end
             if y == 4 then
                 deposit(interfaceStorageMoney.total)
             elseif y == 5 then
@@ -238,6 +249,7 @@ while true do
                 monitor.setCursorPos(4, 10)
                 monitor.write(" 0 < x ")
                 monitor.setBackgroundColor(colors.black)
+                withdrawAmountString = "0"
             end
         elseif screen == "withdraw" then
             local x, y = e[3], e[4]
@@ -285,41 +297,5 @@ while true do
             monitor.write((tonumber(withdrawAmountString) or 0) .. "C")
         end
     end
-    if screen == "info" then
-        print("Info: " .. DisplayedMessage)
-        monitor.clear()
-        local wrappedErrorMessageLines = require "cc.strings".wrap(DisplayedMessage, w)
-        for i, line in ipairs(wrappedErrorMessageLines) do
-            monitor.setCursorPos(1, i)
-            monitor.write(line)
-        end
-        monitor.setCursorPos(1, h)
-        monitor.write("Continue")
-    elseif screen == "main" then
-        checkInterfaceStorage()
-        updateUser()
-        if not currentUser then
-            screen = "invalid"
-        else
-            monitor.clear()
-            monitor.setCursorPos(1, 1)
-            monitor.write(currentUser.name)
-            monitor.setCursorPos(1, 2)
-            monitor.write("Bal: " .. math.floor(currentUser.balance * 100) / 100 .. "C")
-            monitor.setCursorPos(1, 4)
-            monitor.write("deposit " .. math.floor(interfaceStorageMoney.total * 100) / 100 .. "C")
-            monitor.setCursorPos(1, 5)
-            monitor.write("withdraw")
-        end
-    end
-    if screen == "invalid" then
-        monitor.clear()
-        monitor.setCursorPos(1, 1)
-        monitor.write("Invalid card.\nPlease remove card and insert a valid card.")
-    end
-    if screen == "insert" then
-        monitor.clear()
-        monitor.setCursorPos(1, 1)
-        monitor.write("Insert card")
-    end
+    updateUI()
 end
