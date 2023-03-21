5e23ab80f2670fb6189aacb2b4d8c93f70bd70eb
BIueKnight <39872493+BIueKnight@users.noreply.github.com>
2023-03-21 12:41:02 -0500

    Initial Withdraw screen redesign (rough draft)
    Also added in a "did interface storage amount change" check to update the main screen UI only if that value changes, instead of after every event that fires.

--- src/atm.lua
+++ src/atm.lua
@@ -53,7 +53,7 @@ local function countCoins(tab, amount)
 end
 
 local function checkInterfaceStorage()
-    interfaceStorageMoney.total = 0
+    local total = 0
     for slot, item in pairs(interfaceStorage.list()) do
         for key, coin in pairs(bank.coins) do
             if (item.nbt == coin.nbt) then
@@ -61,10 +61,16 @@ local function checkInterfaceStorage()
                     coin = key,
                     count = item.count,
                 }
-                interfaceStorageMoney.total = interfaceStorageMoney.total + item.count * coin.rate
+                total = total + item.count * coin.rate
             end
         end
     end
+
+    local changed = interfaceStorageMoney.total ~= total
+    if (changed) then
+        interfaceStorageMoney.total = total
+    end
+    return changed
 end
 
 local function checkInternalStorage()
@@ -222,9 +228,26 @@ end
 
 -- Event driven handler for listening to events
 local withdrawAmountString = "0"
-
+local spurCnt = 9999
+local bevelCnt = 9999
+local sprocketCnt = 9999
+local cogCnt = 9999
+local crownCnt = 9999
+local sunCnt = 9999
+--[[
+Spurs
+beVels
+sprocKets
+Cogs
+cRowns
+sUns]]
+local withdraw
 local function onEvent(event)
-    checkInterfaceStorage()
+    -- if there was a change to the total in the interface
+    if (checkInterfaceStorage()) then
+        -- update the UI
+        updateUI()
+    end
     -- call api listners first
     local handled = bank.onEvent(event)
 
@@ -261,17 +284,62 @@ local function onEvent(event)
                     monitor.write("Amount:")
                     monitor.setCursorPos(1, 3)
                     monitor.write((tonumber(withdrawAmountString) or 0) .. "C")
-                    monitor.setCursorPos(1, 5)
-                    monitor.write("Confirm")
+
+                    monitor.setCursorPos(7, 5)
+                    monitor.write("S")
+                    monitor.setCursorPos(7, 6)
+                    monitor.write(tostring(spurCnt))
+
+                    monitor.setCursorPos(7, 7)
+                    monitor.write("V")
+                    monitor.setCursorPos(7, 8)
+                    monitor.write(tostring(bevelCnt))
+
+                    monitor.setCursorPos(7, 9)
+                    monitor.write("K")
+                    monitor.setCursorPos(7, 10)
+                    monitor.write(tostring(sprocketCnt))
+
+                    monitor.setCursorPos(12, 5)
+                    monitor.write("C")
+                    monitor.setCursorPos(12, 6)
+                    monitor.write(tostring(cogCnt))
+
+                    monitor.setCursorPos(12, 7)
+                    monitor.write("W")
+                    monitor.setCursorPos(12, 8)
+                    monitor.write(tostring(crownCnt))
+
+                    monitor.setCursorPos(12, 9)
+                    monitor.write("U")
+                    monitor.setCursorPos(12, 10)
+                    monitor.write(tostring(sunCnt))
+
                     monitor.setBackgroundColor(colors.gray)
-                    monitor.setCursorPos(4, 7)
-                    monitor.write(" 1 2 3 ")
-                    monitor.setCursorPos(4, 8)
-                    monitor.write(" 4 5 6 ")
-                    monitor.setCursorPos(4, 9)
-                    monitor.write(" 7 8 9 ")
-                    monitor.setCursorPos(4, 10)
-                    monitor.write(" 0 < x ")
+                    monitor.setCursorPos(8, 5)
+                    monitor.write("<>")
+                    monitor.setCursorPos(8, 7)
+                    monitor.write("<>")
+                    monitor.setCursorPos(8, 9)
+                    monitor.write("<>")
+                    monitor.setCursorPos(13, 5)
+                    monitor.write("<>")
+                    monitor.setCursorPos(13, 7)
+                    monitor.write("<>")
+                    monitor.setCursorPos(13, 9)
+                    monitor.write("<>")
+                    monitor.setCursorPos(10, 1)
+                    monitor.write("Cancel")
+                    monitor.setCursorPos(9, 2)
+                    monitor.write("Confirm")
+                    monitor.setCursorPos(1, 6)
+                    monitor.write("1 2 3")
+                    monitor.setCursorPos(1, 7)
+                    monitor.write("4 5 6")
+                    monitor.setCursorPos(1, 8)
+                    monitor.write("7 8 9")
+                    monitor.setCursorPos(1, 9)
+                    monitor.write("0 < x")
                     monitor.setBackgroundColor(colors.black)
                     withdrawAmountString = "0"
                 end
@@ -285,35 +353,35 @@ local function onEvent(event)
                 end
                 print(x, y)
                 if y == 7 then
-                    if x == 5 then
+                    if x == 1 then
                         withdrawAmountString = withdrawAmountString .. "1"
-                    elseif x == 7 then
+                    elseif x == 3 then
                         withdrawAmountString = withdrawAmountString .. "2"
-                    elseif x == 9 then
+                    elseif x == 5 then
                         withdrawAmountString = withdrawAmountString .. "3"
                     end
                 elseif y == 8 then
-                    if x == 5 then
+                    if x == 1 then
                         withdrawAmountString = withdrawAmountString .. "4"
-                    elseif x == 7 then
+                    elseif x == 3 then
                         withdrawAmountString = withdrawAmountString .. "5"
-                    elseif x == 9 then
+                    elseif x == 5 then
                         withdrawAmountString = withdrawAmountString .. "6"
                     end
                 elseif y == 9 then
-                    if x == 5 then
+                    if x == 1 then
                         withdrawAmountString = withdrawAmountString .. "7"
-                    elseif x == 7 then
+                    elseif x == 3 then
                         withdrawAmountString = withdrawAmountString .. "8"
-                    elseif x == 9 then
+                    elseif x == 5 then
                         withdrawAmountString = withdrawAmountString .. "9"
                     end
                 elseif y == 10 then
-                    if x == 5 then
+                    if x == 1 then
                         withdrawAmountString = withdrawAmountString .. "0"
-                    elseif x == 7 then
+                    elseif x == 3 then
                         withdrawAmountString = withdrawAmountString:sub(1, -2)
-                    elseif x == 9 then
+                    elseif x == 5 then
                         withdrawAmountString = ""
                     end
                 end
@@ -330,6 +398,9 @@ end
 local function main()
     -- run any start methods for the APIs
     bank.onStart()
+    -- initalize UI
+    monitor.setTextScale(0.5)
+    updateUI()
 end
 
 -- intialize, passing main and this onEvent function as the entry listener
